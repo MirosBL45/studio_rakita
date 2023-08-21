7b7243675e33f2f4bc78da4374b22b3f8dc30106
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-21 12:55:35 +0200

    first commit

---
+++ components/TestimonialSlider.jsx
@@ -0,0 +1,32 @@
+// testimonial data
+const testimonialData = [
+  {
+    image: '/t-avt-1.png',
+    name: 'Anne Smith',
+    position: 'Customer',
+    message:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+  },
+  {
+    image: '/t-avt-2.png',
+    name: 'Jane Doe',
+    position: 'Customer',
+    message:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+  },
+  {
+    image: '/t-avt-3.png',
+    name: 'Jhon Doe',
+    position: 'Customer',
+    message:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+  },
+];
+
+function TestimonialSlider() {
+  return (
+    <div>TestimonialSlider</div>
+  )
+}
+
+export default TestimonialSlider
