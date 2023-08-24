1b4d897640c5bd03f4d46671b5e1cf7f4f6af93a
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-24 18:33:18 +0200

    started header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+// next image
+import Image from "next/image";
+
+// next link
+import Link from "next/link";
+
+// components
+import Socials from "../components/Socials";
 
 function Header() {
   return (
-    <header className='absolute z-30 w-full'>Header</header>
-  )
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">Header</div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
