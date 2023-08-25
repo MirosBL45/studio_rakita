33eeb3c15dd6f4da45790cb2b4624b5e992c8a0f
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-25 13:01:22 +0200

    adjust transition

--- components/Transition.jsx
+++ components/Transition.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 // framer motion
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 // variants
 const transitionVariants = {
@@ -18,6 +21,27 @@ const transitionVariants = {
 };
 
 function Transition() {
+  // set state isLandscape
+  const [isLandscape, setIsLandscape] = useState(false);
+
+  // check is device in landscape mode
+  // has to be "globalThis.window?" because just "window" gives error
+  // On the server (where is Next), window is undefined and accessing anything on window will result with error
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLandscape(window.innerWidth > window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // adjust the delay values as needed
+  const transitionDuration = isLandscape ? 0.4 : 3;
+
   return (
     <>
       <motion.div
@@ -26,16 +50,20 @@ function Transition() {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.1, duration: 0.4, ease: "easeInOut" }}
+        transition={{
+          delay: 0.1,
+          duration: 0.4,
+          ease: "easeInOut",
+        }}
       ></motion.div>
-      <motion.div
+      {/* <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
         exit="exit"
         transition={{ delay: 0.15, duration: 0.4, ease: "easeInOut" }}
-      ></motion.div>
+      ></motion.div> */}
       {/* <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
         variants={transitionVariants}
