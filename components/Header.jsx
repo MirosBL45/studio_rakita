2a6c41bbc9586c957ed08bf01f22419276664d2b
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-25 12:00:14 +0200

    header work

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,20 @@ import Socials from "../components/Socials";
 function Header() {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
-      <div className="container mx-auto">Header</div>
+      <div className="container mx-auto">
+        <div>
+          {/* logo */}
+          <Link href={"/"}>
+            <Image
+              src={"/logo.svg"}
+              width={220}
+              height={48}
+              alt="logo"
+              priority={true}
+            />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
