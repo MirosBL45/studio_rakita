35cb3b26b59eb53ce80d57b59f497801f57e5375
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-28 16:49:53 +0200

    about started

--- components/Transition.jsx
+++ components/Transition.jsx
@@ -45,7 +45,7 @@ function Transition() {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-50 bg-[#2e2257]"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
