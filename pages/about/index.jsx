4ecd67e780f1110b0a679535cc05e43b0d64895d
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-29 15:33:09 +0200

    about is still working

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -10,7 +10,7 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
 function About() {
-  const [index, setIndex] = useState(0);
+  const [mainIndex, setMainIndex] = useState(0);
 
   return (
     <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
@@ -26,21 +26,46 @@ function About() {
         <Avatar />
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row">
-        <div>text</div>
-        <div>
-          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+        <div className="flex-1 flex flex-col justify-center">text</div>
+        {/* cards in about section */}
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          {/* for main title of about */}
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 z-[32]">
             {aboutData.map((item, itemIndex) => (
               <div
                 key={itemIndex}
                 className={`${
-                  index === itemIndex &&
-                  "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  mainIndex === itemIndex &&
+                  "text-accent after:w-[100%] after:bg-orange-800 after:transition-all after:duration-300"
                 } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
                 onClick={() => {
-                  setIndex(itemIndex);
+                  setMainIndex(itemIndex);
                 }}
               >
                 {item.title}
+              </div>
+            ))}
+          </div>
+          {/* for content */}
+          <div className="py-2 xl:py-6 flex flex-col gap-y-8 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[mainIndex].info.map((item, itemIndex) => (
+              <div
+                key={itemIndex}
+                className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+              >
+                {/* title */}
+                <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                <div className="hidden md:flex">:</div>
+                {/* years */}
+                {item.stage && <div>{item.stage}</div>}
+                <div className="flex gap-x-4">
+                  {/* icons */}
+                  {item.icons?.map((icon, iconIndex) => (
+                    <div key={iconIndex} className="text-2xl text-white">
+                      {icon}
+                    </div>
+                  ))}
+                </div>
               </div>
             ))}
           </div>
