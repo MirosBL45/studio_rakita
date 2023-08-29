da5b3c64bbdf823536b53319e69f3e9dbb2f2a6a
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-29 11:08:06 +0200

    index for vercel

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -7,10 +7,13 @@ import Circles from "../../components/Circles";
 
 function index() {
   const [index, setIndex] = useState(0);
+  const [paja, setPaja] = useState(2);
 
   return (
     <div>
       <Circles />
+    <p onClick={setIndex(3)}>paja: {paja}</p>
+    <p onClick={setPaja(4)}>index: {index}</p>
     </div>
   );
 }
