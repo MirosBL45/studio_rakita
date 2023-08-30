0a44b587c6f1e34ca4ffad22abd4537878478daf
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-30 18:37:01 +0200

    about more

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -9,6 +9,9 @@ import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
+// counter
+import CountUp from "react-countup";
+
 function About() {
   const [mainIndex, setMainIndex] = useState(0);
 
@@ -26,17 +29,70 @@ function About() {
         <Avatar />
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row">
-        <div className="flex-1 flex flex-col justify-center">text</div>
-        {/* cards in about section */}
+        {/* text part */}
+        <div className="flex-1 flex flex-col justify-center">
+          <h2 className="text-xl mb-3 md:h2">
+            Captivating <span className="text-accent">stories</span> birth
+            magnificent designs.
+          </h2>
+          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+            10 years ago, I began freelancing a sa developer. Since then, I've
+            done remote work for agencies, consulted for startups, and
+            collaborated on digital products for business and consumer use.
+          </p>
+          {/* counters */}
+          <div className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8">
+            <div className="flex flex-1 xl:gap-x-6">
+              {/* expirience */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={10} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Years of expirience
+                </div>
+              </div>
+              {/* clients */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={250} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Satisfied clients
+                </div>
+              </div>
+              {/* projects */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={650} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Finished projects
+                </div>
+              </div>
+              {/* awards */}
+              <div className="relative flex-1">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={8} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Winning awards
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* info - cards in about section */}
         <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
           {/* for main title of about */}
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 z-[32]">
             {aboutData.map((item, itemIndex) => (
+              // after:bg-accent
               <div
                 key={itemIndex}
                 className={`${
                   mainIndex === itemIndex &&
-                  "text-accent after:w-[100%] after:bg-orange-800 after:transition-all after:duration-300"
+                  "text-accent after:w-[100%] after:bg-orange-900 after:transition-all after:duration-300"
                 } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
                 onClick={() => {
                   setMainIndex(itemIndex);
