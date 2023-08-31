8679b5cd2f78d94b43866528bc9b560ea85aff05
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-08-31 22:50:48 +0200

    work started

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -1,53 +1,56 @@
 // data
-const workSlides = {
-  slides: [
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
-  ],
-};
+import { workData } from '../data/workData';
+// icon
+import { BsArrowRight } from 'react-icons/bs';
+
+// next image
+import Image from 'next/image';
+
+// swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// required modules
+import { Pagination } from 'swiper';
 
 function WorkSlider() {
   return (
-    <div>WorkSlider</div>
-  )
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {workData.slides.map((slide, slideIndex) => (
+        <SwiperSlide key={slideIndex}>
+          <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+            {slide.images.map((image, imageIndex) => (
+              <div
+                key={imageIndex}
+                className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+              >
+                <div>
+                  {/* image */}
+                  <Image
+                    src={image.path}
+                    width={500}
+                    height={300}
+                    alt={image.title}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
 
-export default WorkSlider
+export default WorkSlider;
---
+++ data/workData.js
@@ -0,0 +1,45 @@
+// data
+export const workData = {
+    slides: [
+        {
+            images: [
+                {
+                    title: 'title1',
+                    path: '/thumb1.jpg',
+                },
+                {
+                    title: 'title2',
+                    path: '/thumb2.jpg',
+                },
+                {
+                    title: 'title3',
+                    path: '/thumb3.jpg',
+                },
+                {
+                    title: 'title4',
+                    path: '/thumb4.jpg',
+                },
+            ],
+        },
+        {
+            images: [
+                {
+                    title: 'title11',
+                    path: '/thumb4.jpg',
+                },
+                {
+                    title: 'title22',
+                    path: '/thumb1.jpg',
+                },
+                {
+                    title: 'title33',
+                    path: '/thumb2.jpg',
+                },
+                {
+                    title: 'title44',
+                    path: '/thumb3.jpg',
+                },
+            ],
+        },
+    ],
+};
