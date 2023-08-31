a9b902aa25fb11cbbb0a2a8952341a13af2ff8c6
MirosBL45 <jovicmiroslav88@gmail.com>
2023-08-31 17:19:05 +0200

    icons in slider

--- components/ServiceSlider.jsx
+++ components/ServiceSlider.jsx
@@ -1,3 +1,8 @@
+// data
+import { serviceData } from "../data/serviceData";
+// icon
+import { RxArrowTopRight } from "react-icons/rx";
+
 // swiper react components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -6,21 +11,48 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
-// icons
-import {
-  RxCrop,
-  RxDesktop,
-  RxPencil2,
-  RxReader,
-  RxRocket,
-  RxArrowTopRight,
-} from "react-icons/rx";
-
 // required modules
 import { FreeMode, Pagination } from "swiper";
 
 function ServiceSlider() {
-  return <div>ServiceSlider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {serviceData.map((item, itemIndex) => (
+        <SwiperSlide key={itemIndex}>
+          <div>
+            {/* icon */}
+            <div>{item.icon}</div>
+            {/* title & description */}
+            <div>
+              <div>{item.title}</div>
+              <p>{item.description}</p>
+            </div>
+            {/* arrow */}
+            <div className="text-3xl">
+              <RxArrowTopRight />
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
 
 export default ServiceSlider;
