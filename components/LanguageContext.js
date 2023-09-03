ef87ae9a66941990d26bf613ef44b34965cf31f6
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-09-03 12:28:45 +0200

    create LanguageContext

---
+++ components/LanguageContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState, useContext } from "react";
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+    const [language, setLanguage] = useState("eng");
+
+    return (
+        <LanguageContext.Provider value={{ language, setLanguage }}>
+            {children}
+        </LanguageContext.Provider>
+    );
+};
+
+export const useLanguage = () => {
+    return useContext(LanguageContext);
+};
