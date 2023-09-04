d2e35e87aa79b7356730ccbf33c911d35e1fa87a
MirosBL45 <jovicmiroslav88@gmail.com>
2023-09-04 17:29:28 +0200

    work is still working

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -1,21 +1,21 @@
 // data
-import { workData } from '../data/workData';
+import { workData } from "../data/workData";
 // icon
-import { BsArrowRight } from 'react-icons/bs';
+import { BsArrowRight } from "react-icons/bs";
 
 // next image
-import Image from 'next/image';
+import Image from "next/image";
 
 // swiper react components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
 // required modules
-import { Pagination } from 'swiper';
+import { Pagination } from "swiper";
 
 function WorkSlider() {
   return (
@@ -35,7 +35,7 @@ function WorkSlider() {
                 key={imageIndex}
                 className="relative rounded-lg overflow-hidden flex items-center justify-center group"
               >
-                <div>
+                <div className="flex items-center justify-center relative overflow-hidden group">
                   {/* image */}
                   <Image
                     src={image.path}
@@ -43,6 +43,21 @@ function WorkSlider() {
                     height={300}
                     alt={image.title}
                   />
+                  {/* overlay gradient */}
+                  <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-500"></div>
+                  {/* title */}
+                  <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                    <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                      {/* title part 1 */}
+                      <div className="delay-100">title part 1</div>
+                      {/* title part 2 */}
+                      <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                        title part 2
+                      </div>
+                      {/* icon */}
+                      <div>icon</div>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))}
