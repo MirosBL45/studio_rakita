db827970e85bbb1048f75123f00809bbf4109f34
MirosBL45 <jovicmiroslav88@gmail.com>
2023-09-05 18:13:06 +0200

    contact doing

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,5 +1,5 @@
 // components
-import Circles from '../../components/Circles'
+import Circles from "../../components/Circles";
 
 // icons
 import { BsArrowRight } from "react-icons/bs";
@@ -12,15 +12,25 @@ import { fadeIn } from "../../variants";
 
 function Contact() {
   return (
-    <div className='h-full bg-primary/30'>
-      <div className="container mx-auto">
-        <div>
+    <div className="h-full bg-primary/30">
+      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+        {/* text & form */}
+        <div className="flex flex-col w-full max-w-[700px] bg-pink-500/10">
           {/* text */}
-          <h2>Let's connect</h2>
+          <h2 className="h2">
+            Let`s <span className="text-accent">connect.</span>
+          </h2>
+          {/* form */}
+          <form action="">
+            <div className="flex">
+              <input type="text" placeholder="name" className="input" />
+              <input type="email" placeholder="email" className="input" />
+            </div>
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
