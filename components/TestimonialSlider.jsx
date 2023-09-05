cca7b54a5bff6b3cedf071217bab36ebceab63e1
MirosBL45 <jovicmiroslav88@gmail.com>
2023-09-05 16:30:19 +0200

    testimonial starting

--- components/TestimonialSlider.jsx
+++ components/TestimonialSlider.jsx
@@ -1,32 +1,52 @@
-// testimonial data
-const testimonialData = [
-  {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
-  {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
-  {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
-    message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
-  },
-];
+import { testimonialData } from "../data/testimonialData";
+// icon
+import { BsArrowRight } from "react-icons/bs";
+
+// next image
+import Image from "next/image";
+
+// swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// required modules
+import { Pagination } from "swiper";
 
 function TestimonialSlider() {
   return (
-    <div>TestimonialSlider</div>
-  )
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {testimonialData.map((person, slideIndex) => (
+        <SwiperSlide key={slideIndex}>
+          <div>
+            {/* avatar, name, position */}
+            <div>
+              <div>
+                {/* avatar */}
+                <div>avatar</div>
+                {/* name */}
+                <div>name</div>
+                {/* position */}
+                <div>position</div>
+              </div>
+            </div>
+            {/* quote & message */}
+            <div>quote & message</div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
 
-export default TestimonialSlider
+export default TestimonialSlider;
--- pages/testimonials/index.jsx
+++ pages/testimonials/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+// components
+import TestimonialSlider from "../../components/TestimonialSlider";
 
 function Testimonials() {
   return (
-    <div>testimonials</div>
-  )
+    <div>
+      <div>
+        <h2>What clients</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Testimonials
+export default Testimonials;
