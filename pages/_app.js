c55f09af76aada97f1f71876686d92de2a9133ee
MirosBL45 <jovicmiroslav88@gmail.com>
2023-09-08 18:29:46 +0200

    no transition

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { LanguageProvider } from "../components/LanguageContext";
 
 // components
 import Layout from "../components/Layout";
-import Transition from "../components/Transition";
+// import Transition from "../components/Transition";
 
 // router
 import { useRouter } from "next/router";
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <AnimatePresence mode="wait">
           <motion.div key={router.route} className="h-full">
-            <Transition />
+            {/* <Transition /> */}
             <Component {...pageProps} />
           </motion.div>
         </AnimatePresence>
