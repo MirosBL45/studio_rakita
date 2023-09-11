154dcf8d2779b417b1d37b039fd6d44360852bd1
MirosBL45 <jovicmiroslav88@gmail.com>
2023-09-11 13:00:59 +0200

    animation fixing

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ function Home() {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           <motion.div
-            variants={fadeIn("down", 0.3)}
+            variants={fadeIn("down", 0.1)}
             initial="hidden"
             animate="show"
             exit="hidden"
@@ -38,7 +38,7 @@ function Home() {
             <ProjectsBtn />
           </div>
           <motion.div
-            variants={fadeIn("down", 0.4)}
+            variants={fadeIn("down", 0.2)}
             initial="hidden"
             animate="show"
             exit="hidden"
@@ -56,7 +56,7 @@ function Home() {
         <ParticlesContainer />
         {/* avatar img */}
         <motion.div
-          variants={fadeIn("up", 0.5)}
+          variants={fadeIn("up", 0.3)}
           initial="hidden"
           animate="show"
           exit="hidden"
