31e36581c45217f323189edf0954184f02823978
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-01-23 23:46:05 +0100

    some text

--- data/testimonialData.js
+++ data/testimonialData.js
@@ -2,21 +2,21 @@
 export const testimonialData = [
   {
     image: '/t-avt-1.png',
-    name: 'Anne Smith',
+    name: 'Anne Smithovic',
     position: 'Customer',
     message:
       '1Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
   },
   {
     image: '/t-avt-2.png',
-    name: 'Jane Doe',
+    name: 'Jane Peric',
     position: 'Customer',
     message:
       '2Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
   },
   {
     image: '/t-avt-3.png',
-    name: 'Jhon Doe',
+    name: 'Mika Pajic',
     position: 'Customer',
     message:
       '3Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
