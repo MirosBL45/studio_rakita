a98ee158642313fb9cce4d29696ac812898be9a2
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-01-28 23:52:23 +0100

    working about still

--- data/aboutData.js
+++ data/aboutData.js
@@ -92,11 +92,86 @@ export const aboutData = [
   },
 ];
 
+export const aboutDataSR = [
+  {
+    title: "veštine",
+    info: [
+      {
+        title: "srWeb Development",
+        icons: [
+          <FaHtml5 key="html" />,
+          <FaCss3 key="css" />,
+          <FaJs key="js" />,
+          <FaReact key="react" />,
+          <SiNextdotjs key="nextjs" />,
+          <SiFramer key="framer" />,
+          <FaWordpress key="wordpress" />,
+        ],
+      },
+      {
+        title: "srUI/UX Design",
+        icons: [
+          <FaFigma key="figma" />,
+          <SiAdobexd key="adobexd" />,
+          <SiAdobephotoshop key="photoshop" />,
+        ],
+      },
+    ],
+  },
+  {
+    title: "nagrade",
+    info: [
+      {
+        title: "srWebby Awards - Honoree",
+        stage: "2011 - 2012",
+      },
+      {
+        title: "srAdobe Design Achievement Awards - Finalist",
+        stage: "2009 - 2010",
+      },
+    ],
+  },
+  {
+    title: "iskustvo",
+    info: [
+      {
+        title: "srUX/UI Designer - XYZ Company",
+        stage: "2012 - 2023",
+      },
+      {
+        title: "srWeb Developer - ABC Agency",
+        stage: "2010 - 2012",
+      },
+      {
+        title: "srIntern - DEF Corporation",
+        stage: "2008 - 2010",
+      },
+    ],
+  },
+  {
+    title: "kredencijali",
+    info: [
+      {
+        title: "srWeb Development - ABC University, LA",
+        stage: "2011",
+      },
+      {
+        title: "srComputer Science - Technical Institute",
+        stage: "2009",
+      },
+      {
+        title: "srGraphic Designer - ABC Institute, LA",
+        stage: "2006",
+      },
+    ],
+  },
+];
+
 export const aboutDataText =
 {
   title1: 'Studio Rakita.',
   title2: 'Each visual tells story.',
-  description: 'Our organization`s journey has been shaped over numerous years of project involvement filled with satisfaction. It`s a pleasure for you, as you get precisely what you desire, and for us, it`s a joy to engage in what we love. This inspiration led to the formation of a team of individuals enthusiastic about actively participating in capturing the most enthralling videos from your event.',
+  description: 'Our organization`s journey has been shaped over many years of project involvement filled with satisfaction. It`s a pleasure for you, as you get precisely what you desire, and for us, it`s a joy to engage in what we love. This inspiration led to the formation of a team of individuals enthusiastic about actively working in capturing the most enthralling videos from your event.',
   years: {
     text: 'Years of expirience'
   },
--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,5 +1,6 @@
 import {
   aboutData,
+  aboutDataSR,
   aboutDataText,
   aboutDataTextSR,
   years,
@@ -139,45 +140,79 @@ function About() {
         >
           {/* for main title of about */}
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 z-[32]">
-            {aboutData.map((item, itemIndex) => (
-              // after:bg-accent
-              // after:bg-orange-900
-              <div
-                key={itemIndex}
-                className={`${
-                  mainIndex === itemIndex &&
-                  'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
-                } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                onClick={() => {
-                  setMainIndex(itemIndex);
-                }}
-              >
-                {item.title}
-              </div>
-            ))}
+            {language === 'eng'
+              ? aboutData.map((item, itemIndex) => (
+                  <div
+                    key={itemIndex}
+                    className={`${
+                      mainIndex === itemIndex &&
+                      'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                    } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                    onClick={() => {
+                      setMainIndex(itemIndex);
+                    }}
+                  >
+                    {item.title}
+                  </div>
+                ))
+              : aboutDataSR.map((item, itemIndex) => (
+                  <div
+                    key={itemIndex}
+                    className={`${
+                      mainIndex === itemIndex &&
+                      'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                    } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                    onClick={() => {
+                      setMainIndex(itemIndex);
+                    }}
+                  >
+                    {item.title}
+                  </div>
+                ))}
           </div>
           {/* for content */}
           <div className="py-2 xl:py-6 flex flex-col gap-y-4 xl:gap-y-4 items-center xl:items-start">
-            {aboutData[mainIndex].info.map((item, itemIndex) => (
-              <div
-                key={itemIndex}
-                className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
-              >
-                {/* title */}
-                <div className="font-light md:mb-0">{item.title}</div>
-                <div className="hidden md:flex">:</div>
-                {/* years */}
-                {item.stage && <div>{item.stage}</div>}
-                <div className="flex gap-x-4">
-                  {/* icons */}
-                  {item.icons?.map((icon, iconIndex) => (
-                    <div key={iconIndex} className="text-2xl text-white">
-                      {icon}
+            {language === 'eng'
+              ? aboutData[mainIndex].info.map((item, itemIndex) => (
+                  <div
+                    key={itemIndex}
+                    className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                  >
+                    {/* title */}
+                    <div className="font-light md:mb-0">{item.title}</div>
+                    <div className="hidden md:flex">:</div>
+                    {/* years */}
+                    {item.stage && <div>{item.stage}</div>}
+                    <div className="flex gap-x-4">
+                      {/* icons */}
+                      {item.icons?.map((icon, iconIndex) => (
+                        <div key={iconIndex} className="text-2xl text-white">
+                          {icon}
+                        </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
-              </div>
-            ))}
+                  </div>
+                ))
+              : aboutDataSR[mainIndex].info.map((item, itemIndex) => (
+                  <div
+                    key={itemIndex}
+                    className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+                  >
+                    {/* title */}
+                    <div className="font-light md:mb-0">{item.title}</div>
+                    <div className="hidden md:flex">:</div>
+                    {/* years */}
+                    {item.stage && <div>{item.stage}</div>}
+                    <div className="flex gap-x-4">
+                      {/* icons */}
+                      {item.icons?.map((icon, iconIndex) => (
+                        <div key={iconIndex} className="text-2xl text-white">
+                          {icon}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
           </div>
         </motion.div>
       </div>
