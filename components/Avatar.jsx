332c86eab705eb016e329e7573233b1b7522db0c
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-02-01 22:15:35 +0100

    image fixed

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -3,13 +3,14 @@ import Image from 'next/image';
 
 function Avatar() {
   return (
-    <div className="hidden xl:flex xl:max-w-none">
+    <div className="hidden xl:flex xl:max-w-none max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
       <Image
         src={'/drone.svg'}
-        width={737}
+        width={537}
+        // width={737}
         height={678}
         alt="avatar"
-        className="translate-z-0 w-full h-full"
+        className="translate-z-0"
       />
     </div>
   );
