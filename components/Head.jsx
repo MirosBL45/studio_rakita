8accae92812a3f7b25ed99f7d28f849524055f51
MirosBL45 <jovicmiroslav88@gmail.com>
2024-06-27 19:08:04 +0200

    meta tags

--- components/Head.jsx
+++ components/Head.jsx
@@ -4,9 +4,64 @@ function HeadMeta() {
   return (
     <>
       <Head>
-        <meta name="Description" content="Snimite svoje najbitnije dogadjaje" />
-        <title>Studio Rakita | Snimite svoje najbitnije dogadjaje</title>
+        <meta
+          name="description"
+          content="Profesionalno snimanje i fotografisanje svadbi, krštenja, rođendana, i drugih važnih događaja. Pružamo i snimanje fudbalskih, košarkaških i vaterpolo utakmica, ritmičke gimnastike, uz snimanje dronom kao i usluge livestream. Uživajte u najlepšim uspomenama sa profesionalnim kvalitetom."
+        />
+        <meta
+          name="keywords"
+          content="snimanje svadbi, snimanje krštenja, snimanje rođendana, snimanje fudbalskih utakmica, snimanje vaterpolo utakmica, snimanje košarkaških utakmica, snimanje fudbala, snimanje košarke, snimanje vaterpola, snimanje ritmičke gimnastike, livestream utakmica, snimanje dronom, profesionalno snimanje, fotografisanje događaja, video produkcija, snimanje događaja, svadbeni video, sportski događaji, dron snimanje, live prenos, snimanje ceremonija, fotografisanje svadbi, videografija, snimanje sportskih događaja, video montaža"
+        />
+        <meta charset="UTF-8" />
+        <title>
+          Profesionalno Snimanje Svadbi, Krštenja, Sportskih Utakmica | Studio
+          Rakita
+        </title>
+        <meta
+          property="name"
+          content="Profesionalno Snimanje Svadbi, Krštenja, Sportskih Utakmica | Studio Rakita"
+        ></meta>
         <link rel="icon" type="image/x-icon" href="/rakita-studio-logo.ico" />
+        {/* <link rel="canonical" href="https://sajt.com/" /> */}
+        {/* <meta property="url" content="https://sajt.com/" />*/}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Miroslav Jović" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          property="description"
+          content="Profesionalno snimanje i fotografisanje svadbi, krštenja, rođendana, i drugih važnih događaja. Pružamo i snimanje fudbalskih, košarkaških i vaterpolo utakmica, ritmičke gimnastike, uz snimanje dronom kao i usluge livestream. Uživajte u najlepšim uspomenama sa profesionalnim kvalitetom."
+        />
+        <meta
+          property="keywords"
+          content="snimanje svadbi, snimanje krštenja, snimanje rođendana, snimanje fudbalskih utakmica, snimanje vaterpolo utakmica, snimanje košarkaških utakmica, snimanje fudbala, snimanje košarke, snimanje vaterpola, snimanje ritmičke gimnastike, livestream utakmica, snimanje dronom, profesionalno snimanje, fotografisanje događaja, video produkcija, snimanje događaja, svadbeni video, sportski događaji, dron snimanje, live prenos, snimanje ceremonija, fotografisanje svadbi, videografija, snimanje sportskih događaja, video montaža"
+        />
+        <meta
+          property="og:title"
+          content="Profesionalno Snimanje Svadbi, Krštenja, Sportskih Utakmica | Studio Rakita"
+        />
+        <meta
+          property="og:description"
+          content="Profesionalno snimanje i fotografisanje svadbi, krštenja, rođendana, i drugih važnih događaja. Pružamo i snimanje fudbalskih, košarkaških i vaterpolo utakmica, ritmičke gimnastike, uz snimanje dronom kao i usluge livestream. Uživajte u najlepšim uspomenama sa profesionalnim kvalitetom."
+        />
+        {/* <meta property="og:url" content="https://sajt.com/" /> */}
+        <meta property="og:locale" content="sr-Latn-RS" />
+        <meta property="og:image" content="/rakita-studio-logo.ico" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:site_name"
+          content="Profesionalno Snimanje Svadbi, Krštenja, Sportskih Utakmica | Studio Rakita"
+        />
+        <meta
+          name="twitter:title"
+          content="Profesionalno Snimanje Svadbi, Krštenja, Sportskih Utakmica | Studio Rakita"
+        />
+        <meta
+          name="twitter:description"
+          content="Profesionalno snimanje i fotografisanje svadbi, krštenja, rođendana, i drugih važnih događaja. Pružamo i snimanje fudbalskih, košarkaških i vaterpolo utakmica, ritmičke gimnastike, uz snimanje dronom kao i usluge livestream. Uživajte u najlepšim uspomenama sa profesionalnim kvalitetom."
+        />
+        <meta name="twitter:image" content="/rakita-studio-logo.ico" />
+        {/* <meta name="twitter:url" content="https://sajt.com/" /> */}
+        <meta name="twitter:creator" content="Miroslav Jović" />
       </Head>
     </>
   );
