3802d665504ef671f94c6541dbeed394e2e3ccdc
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-06-29 12:17:39 +0200

    meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang="sr-Latn-RS">
             <Head>
-                <meta name="Description" content="Snimite svoje najbitnije dogadjaje" />
+                <meta name="description" content="Profesionalno snimanje i fotografisanje svadbi, krštenja, rođendana, i drugih važnih događaja. Pružamo i snimanje fudbalskih, košarkaških i vaterpolo utakmica, ritmičke gimnastike, uz snimanje dronom kao i usluge livestream. Uživajte u najlepšim uspomenama sa profesionalnim kvalitetom." />
                 <link rel="icon" type="image/x-icon" href="/rakita-studio-logo.ico" />
             </Head>
             <body>
